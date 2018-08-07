f57d96baae400c6e90de97fe87eb839b66538ae3
Tyrfing Mjølner <4lphamal3@gmail.com>
2018-08-08 01:10:54 +0200

    Added from template.
    Added from template.

---
+++ lib/PostgreSQL.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * I never really anything outside vim.
+ * User: Gjermund Gulsland Thorsen( !ghuser TyfringMjolnir )
+ * Date: 8/8/18
+ * Time: UCT 18:00
+ */
+
+namespace DB;
+class PostgreSQL extends DataSource
+{
+    public function execute()
+    {
+        // TODO: Implement execute() method.
+    }
+}
