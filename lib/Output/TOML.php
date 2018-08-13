cf3f1921bcc6d0717bb759979633d68dcd1d6661
Tyrfing Mjølner <4lphamal3@gmail.com>
2018-08-12 20:14:44 -0700

    Create TOML.php

---
+++ lib/Output/TOML.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created in vim.
+ * User: Gjermund G Thorsen( !ghuser TyrfingMjolnir )
+ * Date: 8/12/18
+ * Time: PST 19:21
+ */
+
+namespace TOML; // FileMaker DATA API-like query
+
+
+class TOML extends Output
+{
+    public function execute()
+    {
+        // TODO: Implement execute() method
+    }
+}
