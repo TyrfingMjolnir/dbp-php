b0dd8c88026d9081263b0489ad11f057d002a14b
Tyrfing Mjølner <4lphamal3@gmail.com>
2018-08-12 19:44:28 -0700

    Update FileMakerCWP.php

--- lib/DataSource/FileMakerCWP.php
+++ lib/DataSource/FileMakerCWP.php
@@ -1,9 +1,12 @@
 <?php
 /**
+ * 
  * Created by IntelliJ IDEA.
  * User: Chris Hansen (chris@iviking.org)
  * Date: 9/16/17
  * Time: 8:50 PM
+ * What is this source? FileMaker CWP is a node of the FileMaker server that responds in XML; FMPXMLRESULT, typically used to export/import data.
+ *
  */
 
 namespace DB;
