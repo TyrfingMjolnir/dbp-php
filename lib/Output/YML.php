02e275f58bfd385e88348d0695e73c1736d7b8de
Tyrfing Mjølner <4lphamal3@gmail.com>
2018-08-12 20:16:11 -0700

    Create YML.php

---
+++ lib/Output/YML.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created in vim.
+ * User: Gjermund G Thorsen( !ghuser TyrfingMjolnir )
+ * Date: 8/12/18
+ * Time: PST 19:21
+ */
+
+namespace YML;
+
+
+class YML extends Output
+{
+    public function execute()
+    {
+        // TODO: Implement execute() method
+    }
+}
