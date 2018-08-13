b402c521c23e7c1ac21bc02d378464dcd08065be
Tyrfing Mjølner <4lphamal3@gmail.com>
2018-08-12 19:57:12 -0700

    Create PassThrough.php

---
+++ lib/Request/PassThrough.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created in vim.
+ * User: Gjermund G Thorsen( !ghuser TyrfingMjolnir )
+ * Date: 8/12/18
+ * Time: PST 19:21
+ */
+
+namespace PassThrough; // FileMaker DATA API-like query
+
+
+class PassThrough extends Request
+{
+    public function execute()
+    {
+        // TODO: Implement execute() method to pass request through to DataSource uninterpreted.
+    }
+}
