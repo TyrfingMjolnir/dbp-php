30d5646d08303d3c998e99ebd54ad4efcc4ee8ba
Tyrfing Mjølner <4lphamal3@gmail.com>
2018-08-12 20:15:13 -0700

    Create JSON.php

---
+++ lib/Output/JSON.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created in vim.
+ * User: Gjermund G Thorsen( !ghuser TyrfingMjolnir )
+ * Date: 8/12/18
+ * Time: PST 19:21
+ */
+
+namespace JSON; // FileMaker DATA API-like query
+
+
+class JSON extends Output
+{
+    public function execute()
+    {
+        // TODO: Implement execute() method
+    }
+}
