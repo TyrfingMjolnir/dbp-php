631dd54cb483c1372839077afc81ea3e15505bbf
Tyrfing Mjølner <4lphamal3@gmail.com>
2018-08-12 19:46:34 -0700

    Update FileMakerCDML.php

--- lib/DataSource/FileMakerCDML.php
+++ lib/DataSource/FileMakerCDML.php
@@ -1,9 +1,12 @@
 <?php
 /**
+ *
  * Created in vim.
  * User: Gjermund G Thorsen( !ghuser TyrfingMjolnir )
  * Date: 8/12/18
  * Time: PST 19:21
+ * What is this source? FileMaker CDML is a node of the FileMaker that responds in its own format; typically used to export/import data.
+ *
  */
 
 namespace DB;
