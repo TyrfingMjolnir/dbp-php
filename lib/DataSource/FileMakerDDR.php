1449a89eb21d70f93c6f02f596468eff8bc9869e
Tyrfing Mjølner <4lphamal3@gmail.com>
2018-08-12 19:42:34 -0700

    Update FileMakerDDR.php

--- lib/DataSource/FileMakerDDR.php
+++ lib/DataSource/FileMakerDDR.php
@@ -5,7 +5,7 @@
  * User: Gjermund G Thorsen( !ghuser TyrfingMjolnir )
  * Date: 8/12/18
  * Time: PST 19:21
- * What is this source? FileMaker's Data Design Report is a a set of XML files; typically used to analyze a solution or to CREATE TABLE in a different lanugage; such as SQL.
+ * What is this source? FileMaker's Data Design Report is a a set of XML-files; FMPReport, typically used to analyze a solution or to CREATE TABLE in a different lanugage; such as SQL.
  * 
  */
 
