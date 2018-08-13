91e5fbe6cacde0e66aae002d50a39ddb4fb4e51a
Tyrfing Mjølner <4lphamal3@gmail.com>
2018-08-12 19:45:32 -0700

    Update FileMakerDATAAPI.php

--- lib/DataSource/FileMakerDATAAPI.php
+++ lib/DataSource/FileMakerDATAAPI.php
@@ -4,6 +4,8 @@
  * User: Chris Hansen (chris@iviking.org)
  * Date: 9/16/17
  * Time: 8:50 PM
+ * What is this source? FileMaker DATA API is a node of the FileMaker server that responds in JSON; typically used to export/import data.
+ *
  */
 
 namespace DB;
